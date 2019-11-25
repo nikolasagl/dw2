ee18207bce121f3e939a0b4d9dbe0c5e2881e3d1
Nikolas Lencioni <nikolasagl@hotmail.com>
2019-11-25 14:05:36 -0300

    .

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -13,16 +13,6 @@ routes.post('/register', validators.RegisterValidation.validate('store'), contro
 
 routes.get('/', AuthMiddleware, controllers.UsuarioController.index)
 
-routes.get('/pedido/:id', AuthMiddleware, controllers.PedidoController.show)
-
-routes.get('/pedido/:id/edit', AuthMiddleware, controllers.PedidoController.edit)
-
-routes.put('/pedido/:id', AuthMiddleware, controllers.PedidoController.update)
-
-routes.delete('/pedido/:id', AuthMiddleware, controllers.PedidoController.del)
-
-routes.get('/pedido/:id/restore', AuthMiddleware, controllers.PedidoController.restore)
-
 routes.post('/search', controllers.SearchController.search)
 
 routes.post('/find', controllers.SearchController.find)
--- src/routes/PedidoRoutes.js
+++ src/routes/PedidoRoutes.js
@@ -5,9 +5,20 @@ const routes = express.Router()
 
 const controllers = requireDir('../controllers')
 const validators = requireDir('../validators')
+const AuthMiddleware = require('../middlewares/authMiddleware')
 
 routes.get('/', controllers.PedidoController.index)
 
 routes.post('/', validators.PedidoValidation.validate('store'), controllers.PedidoController.store)
+
+routes.get('/pedido/:id', AuthMiddleware, controllers.PedidoController.show)
+
+routes.get('/pedido/:id/edit', AuthMiddleware, controllers.PedidoController.edit)
+
+routes.put('/pedido/:id', AuthMiddleware, controllers.PedidoController.update)
+
+routes.delete('/pedido/:id', AuthMiddleware, controllers.PedidoController.del)
+
+routes.get('/pedido/:id/restore', AuthMiddleware, controllers.PedidoController.restore)
 
 module.exports = routes
